abf5a941c40863cabf0199d3d27c18d7403932d7
Shaina Chen <shaina.sierra@gmail.com>
2017-07-05 12:21:57 -0700

    Add basic code for react app with text  [#147818703] [https://www.pivotaltracker.com/story/show/147818703]

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
