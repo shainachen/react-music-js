26723e0ce9ab9f57ca709fd56aaa902c8052aacb
Sandra Nissim <sandra.nissim@gmail.com>
2017-07-11 13:59:25 -0700

    Add table of albums on home page and background colors [#147819153]
    Signed-off-by: Shaina Chen <shaina.sierra@gmail.com>

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react';
-const urlForAlbums = 'https://react-music.cfapps.io/albums.json'
+import {Table} from 'pui-react-table';
+const urlForAlbums = 'https://react-music.cfapps.io/albums.json';
+
+const columns = [
+    {
+        attribute: 'name',
+        displayName: 'Album Title',
+        sortable: true
+    },
+    {
+        attribute: 'artist',
+        displayName: 'Artist',
+        sortable: true
+    },
+    {
+        attribute: 'year',
+        displayName: 'Year',
+        sortable: true
+    },
+    {
+        attribute: 'genre',
+        displayName: 'Genre',
+        sortable: true
+    }
+];
 
 class AlbumList extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
         }
     }
@@ -20,19 +44,18 @@ class AlbumList extends Component {
             })
             .then(function(data){
                 console.log(data);
-                that.setState({rawData : JSON.stringify(data)})
+                that.setState({rawData : JSON.stringify(data.albums)});
                 console.log("logger", that.state)
             });
     }
 
     render(){
-
-        if(!this.state.rawData) return <p>Loading...</p>
+        if(!this.state.rawData) return <p>Loading...</p>;
         return (
             <div>
-                <h1>{this.state.rawData}</h1>
+                <Table style={{color: "#243641"}} columns={columns} data={JSON.parse(this.state.rawData)} defaultSort='instances'/>
             </div>
-    )
+        )
     }
 }
 
--- src/App.js
+++ src/App.js
@@ -10,34 +10,13 @@ class App extends Component {
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h2>React Music</h2>
+                    <p className="Table-contents">
+                    <h3>Albums</h3>
                     <AlbumList>AlbumList</AlbumList>
+                    </p>
                 </div>
             </div>
         );
     }
 }
  export default App;
-/*
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>React Music</h2>
-        </div>
-      </div>
-          /*
-        <p className="App-intro">
-            Welcome to React Music
-        </p>
-
-
-      </div>
-
-    );
-  }
-}
-
-export default App;
-*/
